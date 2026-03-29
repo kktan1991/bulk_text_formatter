930c39d90482c2d39f128f271df3963887ec8248
Tan Kien Kiong <65885742+kktan1991@users.noreply.github.com>
2026-03-29 14:55:10 +0800

    Set base URL for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: 'https://kktan1991.github.io/bulk_text_formatter/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
